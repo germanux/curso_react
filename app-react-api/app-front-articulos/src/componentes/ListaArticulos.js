505a27da8fd2d495830fbd1a64f3cf30d26d41ed
German <germanux@gmail.com>
2020-11-20 11:13:28 +0100

    Clase Lista Articulos OK

--- app-react-api/app-front-articulos/src/componentes/ListaArticulos.js
+++ app-react-api/app-front-articulos/src/componentes/ListaArticulos.js
@@ -1,25 +1,46 @@
 import React, { useState, useEffect } from 'react';
 
-export default props => {
-    // No existe this no es una clase
-    // [nombre-variable, setter-variable];
-    let [arrayArticulos, setArrayArticulos] = useState(new Array());
-    //  En vez del component Did mount, usamos useEffect para componentes funcionales
-    useEffect(() => {
-            fetch('http://localhost:4000/api/articulos')    
+export default class ListaArticulos extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state =  {
+            arrayArticulos: []
+        };
+    }
+    componentDidMount() {
+
+        fetch('http://localhost:4000/api/articulos')
             .then(res => res.json())
             .then(arrayData => {
                 // Cuando cambiamos el state, 
                 // React renderiza otra vez precisamente para visualizar ese cambio
-                setArrayArticulos (arrayData);  
-            } );
-    });
-    return (
-    <section>
-        <h1>Articulos</h1>
-        { arrayArticulos.map( (art) => { return (
-<article > { art.titulo }  </article>
-            );
-        }) } 
-    </section>);
-};
+                this.setState( {
+                    arrayArticulos: arrayData
+                } );
+            });
+    }
+    render() {
+        // No existe this no es una clase
+        // [nombre-variable, setter-variable];
+        // let [arrayArticulos, setArrayArticulos] = useState(new Array());
+        //  En vez del component Did mount, usamos useEffect para componentes funcionales
+        
+        return (
+            <section>
+                <h1>Articulos</h1>
+                { this.state.arrayArticulos.map((art, index) => {
+                    if (art.publico) {
+
+                        return (
+                            <article key={index}>
+                                <h1>{art.titulo} </h1>
+                                <p>{art.descripcion}</p>
+                            </article>
+                        );
+                    }
+                })
+                }
+            </section>);
+    }
+}
